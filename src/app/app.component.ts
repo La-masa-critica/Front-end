aae5f359fd7f45257ad2a68e8d48a776a7927091
jorge1b3 <jorge1b3@hotmail.es>
2024-11-06 11:08:03 -0500

    feat: add names and move the delete item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Item } from './item.model';
 import { Category } from './category.model';
 import { catchError, of } from 'rxjs';
 import { CartService } from './sale.service';
-import { Cart, Cart2 } from './cart.model';
+import { Cart } from './cart.model';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -13,27 +13,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  items: Item[] = []; 
+  items: Item[] = [];
   allItems: Item[] = []; // Lista completa de productos para aplicar el filtro
   categories: Category[] = [];
   selectedCategoryId: number | null = null; // Cambiado a null
-  itemIdToShow: Item | null = null; 
-  minPrice: string = ''; 
+  itemIdToShow: Item | null = null;
+  minPrice: string = '';
   maxPrice: string = '';
   total: number = 0;
   cartData: Cart | null = null;
-  cartDatawithNames: Cart2 | null = null;
   profileid: number = 1;
   cartItems: number[] = [];
 
 
-  constructor(private itemService: ItemService, private categoryService: CategoryService,private cartService: CartService, public router: Router) {}
+  constructor(private itemService: ItemService, private categoryService: CategoryService, private cartService: CartService, public router: Router) { }
 
   ngOnInit(): void {
     this.loadItems();
     this.loadCategories();
 
-    this.loadCartwithNames();
+    this.loadNames();
     this.loadCart();
   }
   //Carga productos
@@ -44,9 +43,12 @@ export class AppComponent implements OnInit {
         return of([]);
       })
     ).subscribe(data => {
-      this.allItems = data; 
-      this.items = data; 
+      this.allItems = data;
+      this.items = data;
+      this.items.sort((item1, item2) => item1.id - item2.id);
     });
+    this.loadNames();
+
   }
   //Carga categorias
   loadCategories(): void {
@@ -58,135 +60,141 @@ export class AppComponent implements OnInit {
     ).subscribe(data => {
       this.categories = data;
     });
+    this.loadNames();
   }
   //Define los nuevos productos a mostrar segun categoria
   filterItemsByCategory(): void {
     const categoryId = this.selectedCategoryId ? Number(this.selectedCategoryId) : null;
-    
+
     if (categoryId !== null && categoryId !== 0) {
       this.items = this.allItems.filter(item =>
         item.categoryList.some(category => category.id === categoryId)
       );
     } else {
-      this.items = this.allItems; 
+      this.items = this.allItems;
     }
+    this.loadNames();
   }
 
   //Filtrar por precios con/sin categoria
   filterItems(): void {
-    
-    let categoryIds: number[]  | undefined;
 
-    
+    let categoryIds: number[] | undefined;
+
+
     if (this.selectedCategoryId && this.selectedCategoryId != 0) {
-        categoryIds = [this.selectedCategoryId]; 
+      categoryIds = [this.selectedCategoryId];
     } else {
-        
-        categoryIds = undefined;
+      categoryIds = undefined;
     }
-    
-    
-    const minPrice = this.minPrice ? parseFloat(this.minPrice) : undefined; 
-    const maxPrice = this.maxPrice ? parseFloat(this.maxPrice) : undefined; 
+
+
+    const minPrice = this.minPrice ? parseFloat(this.minPrice) : undefined;
+    const maxPrice = this.maxPrice ? parseFloat(this.maxPrice) : undefined;
     console.log(minPrice);
     console.log(maxPrice);
     console.log(categoryIds);
     console.log(this.categories.map(category => category.id));
-    
+
+
     this.itemService.filterItems(categoryIds, minPrice, maxPrice).subscribe(filteredItems => {
-        if (filteredItems.length === 0) {
-            console.log('No se encontraron productos con los filtros aplicados.');
-        }
-        this.items = filteredItems; 
-    });
-}
-//LOGICA DEL CARRITO A PARTIR DE AQUI
-loadCart(): void {
-  this.cartService.getCartByProfileId(this.profileid).subscribe(cartDatafound => {
-    this.cartData = cartDatafound;
-    console.log(this.cartData);
-  });
-  this.loadCartwithNames();
-}
-
-  
-loadCartwithNames(): void{
-  console.log(this.items);
-  let carritowithname: Cart2 = this.cartData as Cart2;
-  carritowithname?.items.forEach(element => {
-    console.log(element.itemId);
-    element.name = this.items.find(item => item.id === element.itemId)?.name;
-    console.log(element.itemId);
-  });
-  this.cartDatawithNames = carritowithname;
-}
-//Agregar elemento al carrito
-addToCart(itemId: number): void {
-    
-    this.cartService.addCart(this.profileid, itemId, 1).subscribe(
-      cart => { console.log(cart);
-        this.cartData = cart;
+      if (filteredItems.length === 0) {
+        console.log('No se encontraron productos con los filtros aplicados.');
       }
-    );
-    this.loadCartwithNames();
-  } 
-
-  eliminateToCart(itemId: number): void {
-    
-    this.cartService.removeFromCart(this.profileid, itemId).subscribe(() => {});
-    this.actualizar();
-    this.loadCartwithNames();
+      this.items = filteredItems;
+    });
+    this.loadNames();
+  }
+  //LOGICA DEL CARRITO A PARTIR DE AQUI
+  loadCart(): void {
+    this.cartService.getCartByProfileId(this.profileid).subscribe(cartDatafound => {
+      this.cartData = cartDatafound;
+      this.loadNames();
+    });
+    console.log(this.cartData);
+    // this.loadCartwithNames();
   }
 
 
 
-isInCart(itemId: number): boolean {
-  return this.cartItems.includes(itemId);
-}
+  loadNames(): void {
+    this.cartData?.items.forEach(item => {
+      if (item.name === undefined) {
+        item.name = this.items.find(element => element.id === item.itemId)?.name
+      }
+    })
+    this.cartData?.items.sort((item1, item2) => item1.itemId - item2.itemId);
+  }
+  //Agregar elemento al carrito
+  addToCart(itemId: number): void {
 
-async updateCartAndLoad(item: { itemId: number; quantity: number }) {
-  
-  await this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity);
+    this.cartService.addCart(this.profileid, itemId, 1).subscribe(
+      cart => {
+        console.log(cart);
+        this.cartData = cart;
+        this.loadNames();
+      }
+    );
 
-  
-  setTimeout(() => {
-    this.loadCart();
-    this.loadCartwithNames();
-  }, 100); // 500 ms de retraso
-}
+  }
 
-async actualizar() {
-  setTimeout(() => {
-    this.loadCart();
-  }, 100); // 500 ms de retraso
-}
+  eliminateToCart(itemId: number): void {
+
+    this.cartService.removeFromCart(this.profileid, itemId).subscribe(() => { });
+    this.actualizar();
+    this.loadNames();
+  }
 
 
 
-increaseQuantity(item: { itemId: number; quantity: number }): void {
-  item.quantity += 1;
-    this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity).subscribe(() => {}) ;
+  isInCart(itemId: number): boolean {
+    return this.cartItems.includes(itemId);
+  }
+
+  async updateCartAndLoad(item: { itemId: number; quantity: number }) {
+
+    this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity);
+    await this.actualizar();
+
+    // setTimeout(() => {
+    //   this.loadCart();
+    //   this.loadNames();
+    // }, 100); // 500 ms de retraso
+  }
+
+  async actualizar() {
+    setTimeout(() => {
+      this.loadCart();
+      this.loadNames();
+    }, 100); // 500 ms de retraso
+  }
+
+
+
+  increaseQuantity(item: { itemId: number; quantity: number }): void {
+    item.quantity += 1;
+    this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity).subscribe(() => { });
     this.updateCartAndLoad(item);
-    this.loadCartwithNames();
-}
+    this.loadNames();
+  }
 
-decreaseQuantity(item: { itemId: number; quantity: number }): void {
-  if (item.quantity < 1){return}
+  decreaseQuantity(item: { itemId: number; quantity: number }): void {
+    if (item.quantity < 1) { return }
     item.quantity -= 1;
-    this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity).subscribe(() => {}) ;
+    this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity).subscribe(() => { });
     this.updateCartAndLoad(item);
-    this.loadCartwithNames();
-}
+    this.loadNames();
+  }
 
-updateQuantity(item: { itemId: number; quantity: number }): void {
-  this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity).subscribe(() => {});
-  this.updateCartAndLoad(item);
-  this.loadCartwithNames();
-}
+  updateQuantity(item: { itemId: number; quantity: number }): void {
+    this.cartService.updateCartQuantity(this.profileid, item.itemId, item.quantity).subscribe(() => { });
+    this.updateCartAndLoad(item);
+    this.loadNames();
+  }
 
-goToCheckout(): void {
-  this.router.navigate(['/checkout']);
-}
+  goToCheckout(): void {
+    this.router.navigate(['/checkout']);
+  }
 
 
 }
