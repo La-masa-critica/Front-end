491abeab7049dcd75fcde6c8f5ed1a4d44d4ff37
jorge1b3 <jorge1b3@hotmail.es>
2024-11-18 01:06:54 -0500

    remove unused file

--- src/app/app-routing.module.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { MainComponent } from './components/main.component';
-import { CheckoutComponent } from './components/checkout.component';
-
-export const routes: Routes = [
-  { path: '', component: MainComponent },
-  { path: 'checkout', component: CheckoutComponent },
-];
