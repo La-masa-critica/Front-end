a016e1b502b091bddfe7c070e0f03ede69e7475a
jorge1b3 <jorge1b3@hotmail.es>
2024-11-18 01:09:18 -0500

    ✨feat: This is a feature commitinitial approch to api gateway integration. The "localhost" must be changed if the final product run on a docker compose file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
 import { Sale } from '../app/models/sale.model';
 
+const apiUrl = 'http://localhost:8080/api/v1';
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api/v1',
-  itemApiUrl: 'http://localhost:8082/api/v1/item',
-  categoryApiUrl: 'http://localhost:8082/api/v1/category',
-  cartApiUrl: 'http://localhost:8081/api/v1/cart',
-  saleApiUrl: 'http://localhost:8081/api/v1/sale',
+  apiUrl: apiUrl,
+  itemApiUrl: `${apiUrl}/item`,
+  categoryApiUrl: `${apiUrl}/category`,
+  cartApiUrl: `${apiUrl}/cart`,
+  saleApiUrl: `${apiUrl}/sale`,
   currentSale: null as Sale | null,
 };
